7c23c50efb7b15bfc50065709c8006c301c3d508
jamespost <32137126+jamespost@users.noreply.github.com>
2020-06-05 16:26:23 -0500

    first crack at fading in audio
    -not working at all currently

---
+++ Assets/Scripts/Audio Scripts/InteractiveMusicSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveMusicSystem : MonoBehaviour
+{
+    ///a class to handle all interactive music in game
+    ///
+
+    //has access to the audioFades class
+    private audioFades audioFades;
+    //has an audio source for testing purposes
+    [SerializeField] AudioSource testAudioSource;
+
+    private void Start()
+    {
+        audioFades.FadeIn(testAudioSource, 10);
+    }
+
+}
---
+++ Assets/Scripts/Audio Scripts/audioFades.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class audioFades : MonoBehaviour
+{
+    ///a class to handle all audio amplitude fades
+    ///
+
+
+    //the project audio sample rate
+    private int projectSampleRate;  //this should be handled in a parent class eventually
+    //the project audio thread time
+    private double dspTime; //this should be handled in a parent class eventually
+    //[SerializeField] float minAmplitude = 0;
+    //[SerializeField] float maxAmplitude = 1;
+    //values for ADSR points
+    [SerializeField] float attackTime;
+    [SerializeField] float decayTime;
+    
+    [SerializeField] float releaseTime;
+
+    private void Start()
+    {
+        projectSampleRate = AudioSettings.outputSampleRate;
+        dspTime = AudioSettings.dspTime;
+
+        Debug.Log(dspTime);
+    }
+
+
+    //a method to start an audio sources playback with a user defined fade in time
+    public void FadeIn(AudioSource audioSource, float fadeInTime)
+    {
+        float startVolume = 0.002f;
+        audioSource.volume = 0;
+        audioSource.Play();
+
+        while (audioSource.volume < 1)
+        {
+            audioSource.volume += startVolume * Time.deltaTime / fadeInTime;
+            Debug.Log(audioSource.volume);
+        }
+        audioSource.volume = 1;
+        
+    }
+
+    //debug method to print sample rate
+    private void printSampleRate()
+    {
+        Debug.Log(projectSampleRate);
+    }
+}
