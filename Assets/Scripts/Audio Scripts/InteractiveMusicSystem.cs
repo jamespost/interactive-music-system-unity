c1029e06d1a6c703e93dedf92b13031a76b5860b
jamespost <32137126+jamespost@users.noreply.github.com>
2020-06-06 15:56:05 -0500

    more work on audio fades

--- Assets/Scripts/Audio Scripts/InteractiveMusicSystem.cs
+++ Assets/Scripts/Audio Scripts/InteractiveMusicSystem.cs
@@ -1,20 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class InteractiveMusicSystem : MonoBehaviour
 {
     ///a class to handle all interactive music in game
     ///
 
-    //has access to the audioFades class
-    private audioFades audioFades;
-    //has an audio source for testing purposes
-    [SerializeField] AudioSource testAudioSource;
-
-    private void Start()
-    {
-        audioFades.FadeIn(testAudioSource, 10);
-    }
 
 }
